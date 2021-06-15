89a510b1e4fddd90fa50c86fca1377eeb9436cf8
Striker Eureka <pablocaceros@gmail.com>
2021-06-14 23:15:41 -0500

    Se creo el proyecto

---
+++ src/main/java/com/platzimarket/ProyectoParaCrearUnaApiConSpringApplication.java
@@ -0,0 +1,13 @@
+package com.platzimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoParaCrearUnaApiConSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoParaCrearUnaApiConSpringApplication.class, args);
+	}
+
+}
